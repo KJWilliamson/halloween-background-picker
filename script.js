20273c5831b8c5ae540e1b03cecb8ab1281da6e1
kamela williamson <kami63116@yahoo.com>
2022-08-17 13:34:06 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const background_img = document.getElementById("background_img");
+
+const picone_button = document.getElementById("picone_button");
+picone_button.onclick = function () {
+  background_img.className = "picone";
+};
+
+const pictwo_button = document.getElementById("pictwo_button");
+pictwo_button.onclick = function () {
+  background_img.className = "pictwo";
+};
+
+
+const picthree_button = document.getElementById("picthree_button");
+picthree_button.onclick = function () {
+  background_img.className = "picthree";
+};
+
+
+const picfour_button = document.getElementById("picfour_button");
+picfour_button.onclick = function () {
+  background_img.className = "picfour";
+};
+
+const picfive_button = document.getElementById("picfive_button");
+picfive_button.onclick = function () {
+  background_img.className = "picfive";
+};
+
+
+const picsix_button = document.getElementById("picsix_button");
+picsix_button.onclick = function () {
+  background_img.className = "picsix";
+};
+
+
+const picseven_button = document.getElementById("picseven_button");
+picseven_button.onclick = function () {
+  background_img.className = "picseven";
+};
+
+const piceight_button = document.getElementById("piceight_button");
+piceight_button.onclick = function () {
+  background_img.className = "piceight";
+};
+
+
+const picnine_button = document.getElementById("picnine_button");
+picnine_button.onclick = function () {
+  background_img.className = "picnine";
+};
+
+const picten_button = document.getElementById("picten_button");
+picten_button.onclick = function () {
+  background_img.className = "picten";
+};
+
+const piceleven_button = document.getElementById("piceleven_button");
+piceleven_button.onclick = function () {
+  background_img.className = "piceleven";
+};
+
+const pictwelve_button = document.getElementById("pictwelve_button");
+pictwelve_button.onclick = function () {
+  background_img.className = "pictwelve";
+};
+
+const picthirteen_button = document.getElementById("picthirteen_button");
+picthirteen_button.onclick = function () {
+  background_img.className = "picthirteen";
+};
+
+const picfourteen_button = document.getElementById("picfourteen_button");
+picfourteen_button.onclick = function () {
+  background_img.className = "picfourteen";
+};
+
+const picfifteen_button = document.getElementById("picfifteen_button");
+picfifteen_button.onclick = function () {
+  background_img.className = "picfifteen";
+};
+
+const picsixteen_button = document.getElementById("picsixteen_button");
+picsixteen_button.onclick = function () {
+  background_img.className = "picsixteen";
+};
+
+const picseventeen_button = document.getElementById("picseventeen_button");
+picseventeen_button.onclick = function () {
+  background_img.className = "picseventeen";
+};
+
+
